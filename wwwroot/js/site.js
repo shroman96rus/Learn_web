e13b61ffa2146186573f8eb3118934daabb6fe34
Roman Shikhov <shikhov.roman@gmail.com>
2021-06-23 00:36:24 +0500

    Что-то начало получаться с модальными окнами. Something starts to turn out with modal windows

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+showInPopup = (url, title) => {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (res) {
+            $('#modDialog .modal-body').html(res);
+            $('#modDialog .modal-title').html(title);
+            $('#modDialog').modal('show');
+
+        }
+    })
+}
